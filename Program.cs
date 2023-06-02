fa26f9d2426ab4b2327c265b80e9303e83236844
Vedant <vedantnp200@gmail.com>
2023-06-02 22:46:42 +0530

    Linked List is Created

--- Program.cs
+++ Program.cs
@@ -10,23 +10,7 @@ namespace Data_Structure___Linked_List
     {
         public static void Main(string[] args)
         {
-            LinkedList list = new LinkedList();
-            
-            // UC 2 - Insert Nodes
-            /*
-            list.Insert(56);
-            list.Insert(30);
-            list.Insert(70);
 
-            list.Display();
-            */
-
-            // UC3 - Insert node form front
-            list.InsertFront(70);
-            list.InsertFront(30);
-            list.InsertFront(56);
-
-            list.Display();
         }
     }
 }
